ae14bc06303263e0e90b3213f85c02d2c5959bec
Chiemezie Oloto <chiemezieoloto4@gmail.com>
2023-03-31 13:47:04 +0100

    code clean up.

--- ReprocessDLQ/Program.cs
+++ ReprocessDLQ/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ReprocessDLQ;
 
+//Enter the connection string, as applicable
 string connectionString = "";
 string topicName = "";
 string subscriptionName = "";
 await TransferDeadLetterMessages.ProcessTopicAsync(connectionString, topicName, subscriptionName, 10);
+
+//If you wanted to reprocess the DLQ messages in a Service Bus Queue
+//await TransferDeadLetterMessages.ProcessQueueAsync(connectionString: , queueName: );
